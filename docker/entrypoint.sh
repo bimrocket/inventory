cce1963618b604fbaa8bfdbd417b51ee08932ba5
Edu Logroño <elogrono@nexusgeographics.com>
2021-05-24 11:55:29 +0000

    Update docker/entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
+
 if [ -z ${KAFKA_URL} ] || [ -z ${POSTGRES_URL} ] || [ -z ${POSTGRES_USER} ] || [ -z ${POSTGRES_PASSWORD} ]; then
   echo "ERROR: S'han de definir totes les variables d'entorn: KAFKA_URL, POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD"
   exit 1
 fi
+
+echo exec java \
+  -Dkafka.bootstrap.address=${KAFKA_URL} \
+  -Dspring.kafka.bootstrap-servers=${KAFKA_URL} \
+  -Dspring.datasource.url=${POSTGRES_URL} \
+  -Dspring.datasource.username=${POSTGRES_USER} \
+  -Dspring.datasource.password=${POSTGRES_PASSWORD} \
+  -jar ${JAR_FILE} 
 
 exec java \
   -Dkafka.bootstrap.address=${KAFKA_URL} \
@@ -9,4 +19,4 @@ exec java \
   -Dspring.datasource.url=${POSTGRES_URL} \
   -Dspring.datasource.username=${POSTGRES_USER} \
   -Dspring.datasource.password=${POSTGRES_PASSWORD} \
-  -jar ${JAR_FILE} 
+  -jar ${JAR_FILE}
